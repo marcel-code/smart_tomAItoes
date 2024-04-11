c84a62d6901c13feab6501142ebecf546f764431
Marcel Viereck <marcelviereck@Marcels-MacBook-Pro-2.local>
2024-04-11 12:53:42 +0200

    evaluation draft ready - cleaner

--- evaluation.py
+++ evaluation.py
@@ -40,8 +40,6 @@ def get_ground_truth_dict(data):
         for i in range(len(data["name"]))
     }
 
-    # data komt als array raus --> 1x4 array
-
 
 # TODO change back to 4 parameters
 # def get_output_dict(pred, data, key_list=["height", "fw_plant", "leaf_area", "number_of_red_fruits"]):
@@ -57,6 +55,7 @@ def get_output_dict(pred, data, key_list=["height", "fw_plant"]):
     # TODO change the names of res and res_dict to something that makes more sense
     # TODO should we write another function rather than messing up this one?
 
+    # EXPLANATION
     # res is a dictionary build from the tensor pred
     # meaning that the brackets of the tensor get in the way
     # if we convert the tensor pred to a list we can use the list to build the dictionary --> pred.tolist()
@@ -76,40 +75,18 @@ def get_output_dict(pred, data, key_list=["height", "fw_plant"]):
             res_dict[k1] = {k2: v}
     return res_dict
 
-    # data --> vom data loader
-
-    # die zwei arrays in data sind die Formate die in evaluation(conf) reinkommen
-    # result = []
-    # result_dict = {}
-
-
-# evaluation(conf) mit meiner evaluation funktion anpassen
-# model einlesen (DummyModel)
-# mit den gewichten aus dem trainierten Modell kombinieren (pred = ...._...._model_DummyModel_epoch_0)
-# get_data_loader holt aus dem TomatoDataset die Testdaten raus ("test")
-# dann wollen wir die inputs (die Bilder aus dem Testdatensatz) pro Loop 1 Bild einladen
-# der input kommt dann vermutlich ins das Modell und gibt eine Prediction aus (4 Parameter)
-# die 4 Parameter werden dann in ein Dictionary umgewandelt (get_output_dict) und abgespeichert
-# das gleiche wird dann für die ground_truth gemacht (ab diesem Schritt Vorschlag von Copilot für Folgeschritte)
-# dann wird die loss berechnet (ProvidedLoss) und abgespeichert
-# das ganze wird dann für alle Bilder im Testdatensatz gemacht
-# am Ende wird dann der Durchschnitt der Loss berechnet und ausgegeben
-
 
 def evaluation(conf):
     data_conf = copy.deepcopy(conf.data)
     print(data_conf)
 
-    data_loader = TomatoDataset(data_conf)
+    validation_dir = os.path.join(EVAL_PATH, conf.experiment)
 
+    data_loader = TomatoDataset(data_conf)
     test_loader = data_loader.get_data_loader("test")
 
-    # model = DummyModel(conf)
-
-    # TODO change back to get_model function if tested
-    # TODO throw out DummyModel import at top
     model = get_model(conf.model.name)(conf)
-
+    # TODO optimizer not used here
     optimizer = torch.optim.SGD(model.parameters(), lr=conf.train.optimizer.lr, momentum=conf.train.optimizer.momentum)
 
     # TODO discuss with Marcel wether it is enough to load the state_dict as follows without
@@ -121,7 +98,6 @@ def evaluation(conf):
     # model.load_state_dict(checkpoint["model_state_dict"])
     # optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
-    validation_dir = os.path.join(EVAL_PATH, conf.experiment)
     pred = []
     pred_dict = {}
     for i, data in tqdm(enumerate(test_loader), desc="Evaluation"):
@@ -130,17 +106,10 @@ def evaluation(conf):
 
         pred = model(inputs)
         print(pred.tolist())
-        # print(pred)
-
-        # Flatten the list
-        # flat_list = [item for sublist in pred.tolist() for item in sublist]
 
         # pred is given back as a pytorch tensor object and cannot be read in with json
         # it must therefore be converted to a list --> pred.tolist()
-
-        # pred = model(xy) - Take model 20240407_030148_model_DummyModel_epoch_0 (should hopefully work). You need to
-        # load the model class first (DummyModel in this case) and then add the weights stored in the mentioned file
-        # output_dict = get_output_dict(flat_list, data)
+        # TODO delete the print(output_dict) after testing
 
         output_dict = get_output_dict(pred.tolist(), data)
         print(output_dict)
@@ -151,14 +120,6 @@ def evaluation(conf):
                 pred_dict[key].extend(value)
             else:
                 pred_dict[key] = value
-
-    # Convert the model output to a dictionary
-    # Add the output dictionary to the pred_dict
-    # for key in output_dict:
-    # pred_dict[key] = output_dict[key]
-
-    print(output_dict)
-    print(pred_dict)
 
     # Write the results to the submission file
     submission_file = os.path.join(validation_dir, "validation_submission.json")
